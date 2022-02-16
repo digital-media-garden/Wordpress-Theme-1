72ef5edb7ba6ce8a3220e3b712cf8ab14dcd5501
Alex <digital.media.garden@gmail.com>
2022-02-16 17:22:29 -0500

    Code updated

---
+++ archive-car.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * The template for displaying archive pages
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Geniuscourses
+ */
+
+get_header();
+?>
+
+	<div>
+    
+    Template for Custom Post Type Car
+		<header class="page-header">
+			<?php
+			the_archive_title( '<h1 class="page-title">', '</h1>' );
+			the_archive_description( '<div class="archive-description">', '</div>' );
+			?>
+		</header><!-- .page-header -->
+
+
+		<?php if(have_posts()) : while(have_posts()) : the_post();  ?>
+
+			<?php get_template_part('partials/content'); ?>
+
+		<?php  endwhile; else : ?>
+
+			<?php fet_template_part('partials/content', 'none') ?>
+
+		<?php endif; ?>	
+	</div>
+
+<?php
+//get_sidebar();
+get_footer();
